e9237952e7eb7b76f27e558c132e6f3f1a899d27
randomsoul <nextoscar@gmail.com>
2026-09-19 21:16:09 +0530

    Use production canonical host in robots sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/admin/", "/api/"],
     },
-    sitemap: "https://tutorverse.in/sitemap.xml",
+    sitemap: "https://www.tutorverse.in/sitemap.xml",
   };
 }
